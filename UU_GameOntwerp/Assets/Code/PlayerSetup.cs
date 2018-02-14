608fec06ba0b1a39a952d65bf1db81ee921def4a
Cody Bloemhard <cblmhrd@live.nl>
2018-02-14 21:29:59 +0100

    Started networking
    Only player joining etc

---
+++ UU_GameOntwerp/Assets/Code/PlayerSetup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    private Behaviour[] disableThese;
+    private Camera lobbyCam;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < disableThese.Length; i++)
+            {
+                disableThese[i].enabled = false;
+            }
+        }
+        else
+        {
+            lobbyCam = Camera.main;
+            if(lobbyCam != null)
+                lobbyCam.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (lobbyCam != null)
+            lobbyCam.gameObject.SetActive(true);
+    }
+}
