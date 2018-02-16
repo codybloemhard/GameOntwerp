df49640693492d1e3bd394741fa3aed8386cf481
Cody Bloemhard <cblmhrd@live.nl>
2018-02-16 21:25:55 +0100

    Blockspawn synced over lan
    ok

--- UU_GameOntwerp/Assets/Code/BlockSpawner.cs
+++ UU_GameOntwerp/Assets/Code/BlockSpawner.cs
@@ -1,27 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class BlockSpawner : MonoBehaviour {
-
+public class BlockSpawner : NetworkBehaviour
+{
     [SerializeField]
     private GameObject cubeObj;
     [SerializeField]
     private GameObject closestSpawnPoint;
-
 
     void Start () {
 		
 	}
 	
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (cubeObj != null)
-            {
-                Vector3 spawnPos = closestSpawnPoint.transform.position;
-                GameObject spawned = (GameObject)Instantiate(cubeObj, spawnPos, Quaternion.identity);
-            }
-        }
+        if (Input.GetMouseButtonDown(0) && isLocalPlayer)
+            CmdSpawnCube();
 	}
+    
+    [Command]
+    void CmdSpawnCube()
+    {
+        if (cubeObj != null)
+        {
+            Vector3 spawnPos = closestSpawnPoint.transform.position;
+            GameObject spawned = (GameObject)Instantiate(cubeObj, spawnPos, Quaternion.identity);
+            NetworkServer.Spawn(spawned);
+        }
+    }
 }
