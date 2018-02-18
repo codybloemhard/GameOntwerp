3e37601bcaa3cec1d63f3cb1e47bec9f52a98618
Cody Bloemhard <cblmhrd@live.nl>
2018-02-18 13:28:42 +0100

    restore master
    ok

---
+++ UU_GameOntwerp/Assets/Code/Center.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public enum Phase
+{
+    NONE,
+    BUILDING,
+    PLAYING
+}
+
+public class Center : NetworkBehaviour {
+
+    public static Center instance;
+    //all networked vars
+    [SerializeField]
+    [SyncVar]
+    public Phase phase;
+    [SyncVar]
+    private float timer;
+    //editable vars
+    [SerializeField]
+    private int roundTime = 30;
+
+    private void Awake () {
+        if (instance != null)
+            Destroy(this);
+        else instance = this;
+        phase = Phase.BUILDING;
+	}
+	
+	private void Update () {
+        if (!isServer) return;
+        timer += Time.deltaTime;
+        if (timer >= roundTime)
+        {
+            SwitchMode();
+            timer = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            SwitchMode();
+            timer = 0f;
+        }
+	}
+
+    private void SwitchMode()
+    {
+        if (phase == Phase.BUILDING) phase = Phase.PLAYING;
+        else if (phase == Phase.PLAYING) phase = Phase.BUILDING;
+    }
+
+    public float GetTimeLeft()
+    {
+        return roundTime - timer;
+    }
+}
---
+++ UU_GameOntwerp/Assets/Code/PlayerSetup.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    private Behaviour[] localOff_bhv, externOff_bhv;
+    [SerializeField]
+    private GameObject[] localOff_obj, externOff_obj;
+    private Camera lobbyCam;
+    private Phase lastPhase;
+    //components on this object
+    private Rigidbody body;
+    private CapsuleCollider collider;
+    [SerializeField]
+    private Behaviour physicsControls, flyControls, blockSpawner, shooting;
+
+    private void Start()
+    {
+        lastPhase = Phase.NONE;
+        body = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
+        InitNet();
+        
+        if (isLocalPlayer)
+        {
+            lobbyCam = Camera.main;
+            if (lobbyCam != null)
+                lobbyCam.gameObject.SetActive(false);
+        }
+    }
+    //enable and disable the correct behaviours and objects based on if were the local player
+    private void InitNet()
+    {
+        if (isLocalPlayer)
+        {
+            for (int i = 0; i < localOff_bhv.Length; i++)
+                localOff_bhv[i].enabled = false;
+            for (int i = 0; i < localOff_obj.Length; i++)
+                localOff_obj[i].SetActive(false);
+        }
+        else
+        {
+            for (int i = 0; i < externOff_bhv.Length; i++)
+                externOff_bhv[i].enabled = false;
+            for (int i = 0; i < externOff_obj.Length; i++)
+                externOff_obj[i].SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (lobbyCam != null)
+            lobbyCam.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        Phase currentPhase = Center.instance.phase;
+        if (currentPhase != lastPhase)
+        {
+            lastPhase = currentPhase;
+            if (currentPhase == Phase.BUILDING)
+            {
+                body.useGravity = false;
+                collider.enabled = false;
+                physicsControls.enabled = false;
+                flyControls.enabled = true;
+                blockSpawner.enabled = true;
+				shooting.enabled = false;
+            }
+            else if (currentPhase == Phase.PLAYING)
+            {
+                body.useGravity = true;
+                collider.enabled = true;
+                physicsControls.enabled = true;
+                flyControls.enabled = false;
+                blockSpawner.enabled = false;
+				shooting.enabled = true;
+            }
+            InitNet();
+        }
+        if(currentPhase == Phase.BUILDING)
+        {
+            body.velocity = Vector3.zero;
+            if (transform.position.y < 0f)
+                transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        }
+    }
+}
