67a81ce396ece593f35f356c34ca2ed3ace8e1b0
Cody Bloemhard <cblmhrd@live.nl>
2018-02-18 12:42:15 +0100

    RECOMMIT "Global state sync(phase etc) and UI"
    jezuz

---
+++ UU_GameOntwerp/Assets/Code/UI_PhaseIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PhaseIndicator : MonoBehaviour {
+
+    private Image image;
+    public Sprite buildImg, fightImg;
+
+    private void Start () {
+        image = GetComponent<Image>();
+	}
+
+    private void Update () {
+        if (image == null)
+        {
+            Debug.Log("Image is NULL in UI_PhaseIndicator");
+            return;
+        }
+        if (Center.instance.phase == Phase.NONE)
+            image.sprite = null;
+        else if (Center.instance.phase == Phase.BUILDING)
+            image.sprite = buildImg;
+        else if (Center.instance.phase == Phase.PLAYING)
+            image.sprite = fightImg;
+    }
+}
